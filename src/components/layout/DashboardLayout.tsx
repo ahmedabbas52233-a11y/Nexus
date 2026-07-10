30c997f55b8befb52c5052105860c10f3b1e7665
ahmedabbas52233-a11y <ahmedabbas52233@gmail.com>
2026-07-11 02:41:55 +0500

    fix: files and accessibilty issues

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
 import { Navbar } from './Navbar';
 import { Sidebar } from './Sidebar';
 
